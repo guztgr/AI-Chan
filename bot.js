bea6d015367e612acdf2972b412d3d1a48e0d4c7
guztgr <39617914+guztgr@users.noreply.github.com>
2018-07-07 15:22:19 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,28 +11,16 @@ client.on('ready', () => {
 });
 // response command =========================================
 client.on("message", (message) => {
-    // Exit and stop if it's not there ======================
-    client.on("message", (message) => {
-        if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-      
-        if (message.content.startsWith(config.prefix + "ping")) {
-          message.channel.send("pong!");
-        } else
-        if (message.content.startsWith(config.prefix + "foo")) {
-          message.channel.send("bar!");
-        }
-      });
-      if(message.content.startsWith(config.prefix + "prefix")) {
-        // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
-        let newPrefix = message.content.split(" ").slice(1, 2)[0];
-        // change the configuration in memory
-        config.prefix = newPrefix;
-      
-        // Now we have to save the file.
-        fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
-      }
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  
+    if (message.content.startsWith(config.prefix + "ping")) {
+      message.channel.send("pong!");
+    } else
+    if (message.content.startsWith(config.prefix + "foo")) {
+      message.channel.send("bar!");
+    }
+  });
 // ini akses token ==========================================
 // masuk ke pengaturan heroku setting "BOT_TOKEN : token"
 client.login(config.token);
 //============= end code =====================================
-
