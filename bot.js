e6d57fba50f4e07aa79dd6f15ee2a6ef7e540413
guztgr <39617914+guztgr@users.noreply.github.com>
2018-07-07 14:43:15 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,27 @@
+// require ==================================================
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client  = new Discord.Client();
+const prefix  = "!"; // Set the prefix
+//===========================================================
 
+// console ==================================================
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// ini akses token ===================================
+// response command =========================================
+client.on("message", (message) => {
+    // Exit and stop if it's not there
+    if (!message.content.startsWith(prefix)) return;
+  
+    if (message.content.startsWith(prefix + "ping")) {
+      message.channel.send("pong!");
+    } else
+    if (message.content.startsWith(prefix + "foo")) {
+      message.channel.send("bar!");
+    }
+  });
+// ini akses token ==========================================
 // masuk ke pengaturan heroku setting "BOT_TOKEN : token"
 client.login(process.env.BOT_TOKEN);
+//============= end code =====================================
+
